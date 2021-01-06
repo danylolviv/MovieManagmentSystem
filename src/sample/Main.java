db77bd73177dece6ac798e622eb793164d9746d8
Asyno <47446626+Asynoo@users.noreply.github.com>
2021-01-06 22:28:30 +0100

    Coolio

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Gui/Views/MainView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("MMS");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
