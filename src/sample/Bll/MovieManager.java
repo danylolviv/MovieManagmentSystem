a6c6d556dc4622ad7cc2a768de10afbfeb02cebf
Danylo <danyloboshagora@gmail.com>
2021-01-08 18:33:39 +0100

    Tried to establish connection to movie manager but faced the Movie object which doesnt have all the necessary staff I need so will need to change it at some point later.

--- src/sample/Bll/MovieManager.java
+++ src/sample/Bll/MovieManager.java
@@ -1,4 +1,13 @@
 package sample.Bll;
 
+import sample.Be.Movie;
+
 public class MovieManager {
+
+    public void addMovie(Movie movie){
+
+    }
+
 }
+
+
--- src/sample/Gui/Models/MovieModel.java
+++ src/sample/Gui/Models/MovieModel.java
@@ -1,13 +1,19 @@
 package sample.Gui.Models;
 
+import sample.Be.Movie;
+import sample.Bll.MovieManager;
+
 import java.time.LocalDate;
 
 public class MovieModel {
+
+    public MovieManager movieManager;
+
     MovieModel(){
 
     }
 
     public void addMovie(String title, String year, LocalDate releaseDate, String Description, String moviePath){
-
+        //movieManager.addMovie(new Movie(1, title, ));
     }
 }
